99efeb87a11cef460c79b00d7e6d70c9a00a8fbc
swati082001 <swatisucharitamohanty1@gmail.com>
2023-02-20 21:16:19 +0530

    margins

--- src/Components/SKILLS/Skills.jsx
+++ src/Components/SKILLS/Skills.jsx
@@ -13,67 +13,67 @@ const Skills = () => {
 
         <Fade cascade direction='left'>
 
-        <Box w="90%" margin={"auto"} p={5} mt={{base:"50px",sm:"50px",md:"50px",lg:"40px"}} h="auto" border="1px solid white" borderRadius="15px" textAlign="center" >
+        <Box w="90%" margin={"auto"} p={5} mt={{base:"50px",sm:"50px",md:"50px",lg:"40px"}}  h="auto" border="1px solid white" borderRadius="15px" textAlign="center" >
 
           <Box p={7}>
-          <Grid  templateColumns={{base:"repeat(2,1fr)",sm:"repeat(3,1fr)",md:"repeat(3,1fr)",lg:"repeat(4,1fr)"}} gap={{base:"15px",sm:"15px",md:"15px",lg:"20px"}}>
-          <Box >
-          <Image w={{base:"60px",sm:"70px",md:"80px",lg:"100px"}} ml={{base:"20px",sm:"40px",md:"60px",lg:"70px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/html-5.svg' alt='' />
+          <Grid templateColumns={{base:"repeat(2,1fr)",sm:"repeat(3,1fr)",md:"repeat(3,1fr)",lg:"repeat(4,1fr)"}} gap={{base:"15px",sm:"15px",md:"15px",lg:"20px"}}>
+          <Box  >
+          <Image w={{base:"60px",sm:"70px",md:"80px",lg:"100px"}} ml={{base:"0px",sm:"0px",md:"30px",lg:"30px"}}  mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/html-5.svg' alt='' />
           
           </Box>
 
           <Box >
-          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"20px",sm:"40px",md:"60px",lg:"70px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/css3.svg' alt='' />
+          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"0px",sm:"0px",md:"30px",lg:"30px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/css3.svg' alt='' />
           
           </Box>
 
           <Box >
-          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"20px",sm:"40px",md:"60px",lg:"70px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/javascript.svg' alt='' />
+          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"0px",sm:"0px",md:"30px",lg:"30px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/javascript.svg' alt='' />
           
           </Box>
 
           <Box >
-          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"20px",sm:"40px",md:"60px",lg:"70px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/reactjs.svg' alt='' />
+          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"0px",sm:"0px",md:"30px",lg:"30px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/reactjs.svg' alt='' />
           
           </Box>
 
           <Box >
-          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"20px",sm:"40px",md:"60px",lg:"70px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/redux.svg' alt='' />
+          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"0px",sm:"0px",md:"30px",lg:"30px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/redux.svg' alt='' />
           
           </Box>
 
           <Box >
-          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"20px",sm:"40px",md:"60px",lg:"70px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/nodejs.svg' alt='' />
+          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"0px",sm:"0px",md:"30px",lg:"30px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/nodejs.svg' alt='' />
           
           </Box>
 
           <Box >
-          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"20px",sm:"40px",md:"60px",lg:"70px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/mongodb.svg' alt='' />
+          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"0px",sm:"0px",md:"30px",lg:"30px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/mongodb.svg' alt='' />
           
           </Box>
 
           <Box >
-          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"20px",sm:"40px",md:"60px",lg:"70px"}} mt="10px" src='https://gayathry-portfolio.vercel.app/ts.png' alt='' />
+          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"0px",sm:"0px",md:"30px",lg:"30px"}} mt="10px" src='https://gayathry-portfolio.vercel.app/ts.png' alt='' />
           
           </Box>
 
           <Box >
-          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"20px",sm:"40px",md:"60px",lg:"70px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/git.svg' alt='' />
+          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"0px",sm:"0px",md:"30px",lg:"30px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/git.svg' alt='' />
           
           </Box>
 
           <Box >
-          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"20px",sm:"40px",md:"60px",lg:"70px"}} mt="10px"  src='https://img.icons8.com/color/70/null/chakra-ui.png' alt='' />
+          <Image w={{base:"65px",sm:"60px",md:"80px",lg:"100px"}} ml={{base:"0px",sm:"0px",md:"30px",lg:"30px"}} mt="10px"  src='https://img.icons8.com/color/70/null/chakra-ui.png' alt='' />
          
           </Box>
 
           <Box >
-          <Image w={{sm:"60px",md:"80px",lg:"100px"}} ml={{base:"20px",sm:"40px",md:"60px",lg:"70px"}} mt="10px"  src='https://chiranjeev-thapliyal.vercel.app/svg/material-ui.svg' alt='' />
+          <Image w={{sm:"60px",md:"80px",lg:"100px"}} ml={{base:"0px",sm:"0px",md:"30px",lg:"30px"}} mt="10px"  src='https://chiranjeev-thapliyal.vercel.app/svg/material-ui.svg' alt='' />
           
           </Box>
 
           <Box >
-          <Image w={{sm:"60px",md:"80px",lg:"100px"}} ml={{base:"20px",sm:"40px",md:"60px",lg:"70px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/mysql-logo.svg' alt='' />
+          <Image w={{sm:"60px",md:"80px",lg:"100px"}} ml={{base:"0px",sm:"0px",md:"30px",lg:"30px"}} mt="10px" src='https://chiranjeev-thapliyal.vercel.app/svg/mysql-logo.svg' alt='' />
           
           </Box>
 
