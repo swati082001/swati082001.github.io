d2e436cedd1fb2e16cf1386b78024ae5c8c66e7a
Swati Mohanty <swatisucharitamohanty1@gmail.com>
2023-04-13 13:23:40 +0530

    contact about and skills added

--- src/Components/ABOUT/About.jsx
+++ src/Components/ABOUT/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Styles from "./About.module.css";
-import {Box, Flex,Text} from "@chakra-ui/react";
+import {Box, Flex,ListItem,Text, UnorderedList} from "@chakra-ui/react";
 import { Fade } from "react-awesome-reveal";
 
 const About = () => {
@@ -14,11 +14,22 @@ const About = () => {
             
                 </Box>
 
-                <Box w={{base:"70%",sm:"80%",md:"80%",lg:"50%"}}>
+                <Box w={{base:"90%",sm:"90%",md:"90%",lg:"65%"}}>
                     <Text textStyle={"AboutHead"}>ABOUT ME</Text>
-                    <Text textStyle="AboutPara">A self-motivated, hard-working, and ambitious Web Developer with proficiency in MERN Stack. Developing websites using Javascript, React, Redux, and Chakra UI. 1200+ hours of coding and hands-on experience in developing various Web-Apps and programs. Looking forward to working as an accountable and competent employee in an exciting tech company.
-                    </Text>
-            
+                    <UnorderedList>
+                      <ListItem>
+                      <Text textStyle="AboutPara">  Hi, I'm Swati Mohanty, a recent graduate with a degree in Geology and a passion for full-stack web development. I have experience in front-end technologies like HTML, CSS, and JavaScript, and have also worked with back-end technologies like Node JS, Express, and MongoDB. 1200+ hours of coding and hands-on experience in developing various Web-Apps and programs. My projects have involved building responsive and user-friendly websites using frameworks like React and Redux. </Text>
+                      </ListItem>
+
+                      <ListItem>
+                      <Text textStyle="AboutPara"> I am a hardworking and self-motivated individual with a strong commitment to learning and improving my skills. I am constantly exploring new technologies and tools to expand my knowledge and stay up-to-date with the latest trends in the industry. My approach to web development is focused on creating solutions that are simple, elegant, and efficient, while also delivering a great user experience.</Text>
+                      </ListItem>
+
+                      <ListItem>
+                      <Text textStyle="AboutPara">In my free time, I enjoy reading and painting, which helps me stay creative and inspired. If you're looking for a full-stack web developer who is passionate, enthusiastic, and always willing to learn, I would love to connect and discuss your projects. </Text>
+                      </ListItem>
+                    </UnorderedList>
+          
                 </Box>
             </Flex>
             </Fade>
--- src/Components/CONTACT/Contact.jsx
+++ src/Components/CONTACT/Contact.jsx
@@ -12,7 +12,14 @@ const Contact = () => {
            
             <Flex>
 
-            <Box w="80%" margin={"auto"} p={5} mt={{base:"50px",sm:"50px",md:"50px",lg:"40px"}} h="auto" border="1px solid white" borderRadius="15px" >
+            <Box w="80%" margin={"auto"} p={5} mt={{base:"50px",sm:"50px",md:"50px",lg:"40px"}} h="auto" border="1px solid white" borderRadius="15px" display={"flex"} flexDirection={{base:"column",sm:"column", md:"column",lg:"row"}} justifyContent={"center"} alignItems={"center"}>
+
+            <Box  w={{base:"100%",sm:"100%",md:"100%",lg:"100%"}} >
+              <Image  borderRadius={"15px"} w={{base:"100%",sm:"100%",md:"100%",lg:"80%"}} src='https://visme.co/blog/wp-content/uploads/2020/02/header-1200.gif' alt=''/>
+            </Box>
+
+            <Box  w={{base:"100%",sm:"100%",md:"100%",lg:"60%"}}>
+
             <Text textStyle="AboutSubHead" mt={{sm:"40px",md:"40px",lg:"30px"}}>Contact Me</Text>
              <Text textAlign={{base:"center",sm:"center",md:"center",lg:"initial"}}>Contact Number - +91 8018531080</Text>
              <Text textAlign={{base:"center",sm:"center",md:"center",lg:"initial"}}>Email Id- swatisucharitamohanty1@gmail.com</Text>
@@ -23,11 +30,11 @@ const Contact = () => {
             
             <a href='https://github.com/swati082001'><Image w={{base:"30px",sm:"50px",md:"50px",lg:"50px"}} ml="35px" borderRadius="10px" bg="#b107ff" src='https://iconsplace.com/wp-content/uploads/_icons/000000/256/png/github-icon-256.png' alt='linked in logo' /></a>
              </Flex>
-            
             </Box>
             
+            </Box>
 
-            
+           
             </Flex>
 
         
